a895f4ba3e82dd0c1a3f6b9c681d9360c12f6d96
Amol shelke <shelkeamol897@gmail.com>
2022-11-26 12:05:27 +0530

    Adding TodoForm component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from 'react';
+
 import './App.css';
+import TodoForm from './components/todoForm';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TodoForm />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/todoForm/TodoForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const TodoForm = () => {
+  return (
+    <div className="todo-form">
+      <h1>Let's creaate a Todo App</h1>
+    </div>
+  );
+};
