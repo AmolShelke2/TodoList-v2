63a2ccd3e88e05944e729aeb9adf6df65b85dabb
Amol shelke <shelkeamol897@gmail.com>
2022-11-26 16:41:31 +0530

    Adding Delete todo feature

--- src/components/todoLists/TodoItem.js
+++ src/components/todoLists/TodoItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 export const TodoItem = props => {
+  const deleteHandler = () => {
+    console.log('deleting');
+  };
+
   return (
     <div className="todo-item" key={props.id}>
       <p>{props.task}</p>
-      <button className="removeBtn">x</button>
+      <button className="removeBtn" onClick={deleteHandler}>
+        x
+      </button>
     </div>
   );
 };
