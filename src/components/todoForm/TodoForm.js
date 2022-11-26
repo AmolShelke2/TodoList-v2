f7b9885191bbc2047c5c022182c6f452e7f5f3c3
Amol shelke <shelkeamol897@gmail.com>
2022-11-26 12:25:48 +0530

    Added todoForm component with input handlers and useState hook

--- src/components/todoForm/TodoForm.js
+++ src/components/todoForm/TodoForm.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const TodoForm = () => {
+  const [enteredTodo, setEnteredTodo] = useState('');
+
+  const enterdTodoChangeHandler = e => {
+    setEnteredTodo(e.target.value);
+  };
+
+  const addTodoHandler = e => {
+    e.preventDefault();
+    //     console.log(enteredTodo);
+
+    if (enteredTodo.length === 0 || enteredTodo === '') {
+      console.error('Add a valid todo');
+      return;
+    }
+
+    const todoToBeAdded = {
+      todoItem: enteredTodo,
+      id: Math.random().toString(),
+    };
+    console.log(todoToBeAdded);
+
+    setEnteredTodo('');
+  };
+
   return (
     <div className="todo-form">
-      <h1>Let's creaate a Todo App</h1>
+      <form onSubmit={addTodoHandler}>
+        <label htmlFor="text">Enter Todo</label>
+        <input
+          type="text"
+          onChange={enterdTodoChangeHandler}
+          value={enteredTodo}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
     </div>
   );
 };
